7187accf7478926d005f6ada642733a74d50dc34
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-06-13 16:10:08 +0800

    配置数据库链接

--- config/config.go
+++ config/config.go
@@ -1,1 +1,19 @@
 package config
+
+// 配置数据库链接
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/todolist?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn),&gorm.Config{})
+	if err != nil {
+		log.Fatal("数据库连接失败", err)
+	}
+}
