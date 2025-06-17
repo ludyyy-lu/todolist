82cfd1e4d627bc4f3c5d363395f51f41806f2531
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-06-17 15:16:02 +0800

    完善hash—util部分 | 删除多余的.sql文件

--- controllers/authController.go
+++ controllers/authController.go
@@ -7,7 +7,6 @@ import (
 	"todolist/utils"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(c *gin.Context) {
@@ -26,11 +25,11 @@ func Register(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "用户名已存在"})
 	}
 	//加密
-	hashed, _ := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
-	input.Password = string(hashed)
+	hashed, _ := utils.HashPassword(input.Password)
+	input.Password = hashed
 
 	if err := config.DB.Create(&input).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error":"注册失败，用户名可能重复"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "注册失败，用户名可能重复"})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "注册成功"})
@@ -38,7 +37,7 @@ func Register(c *gin.Context) {
 
 func Login(c *gin.Context) {
 	var input models.User
-	if err := c.ShouldBindJSON(&input); err!= nil {
+	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "请求参数错误"})
 		return
 	}
@@ -46,16 +45,16 @@ func Login(c *gin.Context) {
 	var user models.User
 	if err := config.DB.Where("username = ?", input.Username).First(&user).Error; err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "用户未注册"})
-		return	
+		return
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(input.Password)); err != nil {
+	if err := utils.CheckPasswordHash(input.Password, user.Password); err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "密码错误"})
 		return
 	}
 
 	token, err := utils.GenerateToken(user.ID)
-	if err!= nil {
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "生成token失败"})
 		return
 	}
--- utils/hash.go
+++ utils/hash.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password),14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash),[]byte(password))
+	if err != nil {
+		return errors.New("密码错误")
+	}
+	return nil
+}
